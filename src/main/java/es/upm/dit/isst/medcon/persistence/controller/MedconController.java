8d5fdc1d1c3b02c85f6973da85b9b4e6e8ef6a24
Ramón Hernández García <rmnhg16@gmail.com>
2022-03-30 15:44:16 +0200

    Datos de consulta de ejemplo Se han añadido datos de consultas a modo de ejemplo tal y como estaban en el FrontEnd originalmente.

--- src/main/java/es/upm/dit/isst/medcon/persistence/controller/MedconController.java
+++ src/main/java/es/upm/dit/isst/medcon/persistence/controller/MedconController.java
@@ -25,6 +25,19 @@ public class MedconController {
     public static final Logger log = LoggerFactory.getLogger(MedconController.class);
     public MedconController(MedconRepository t) {
         this.medconRepository = t;
+        this.medconRepository.save(new Consulta( 7 ,"30/3/2022", "Ramón","María Asunción Santamaría Galdón", "Consultas de prueba","A01", false));
+        this.medconRepository.save(new Consulta( 8 ,"30/3/2022", "Ramón","Jesús Frayle Ardanuy", "Consultas de prueba","D03", false));
+        this.medconRepository.save(new Consulta( 9 ,"30/3/2022", "Ramón","Víctor Abraham Villagrá González", "Consultas de prueba","H69", false));
+        this.medconRepository.save(new Consulta( 10 ,"30/3/2022", "Ramón","Gabriel Huecas Toribio", "Consultas de prueba","X43", false));
+        this.medconRepository.save(new Consulta( 11 ,"30/3/2022", "Ramón","María de la Nava Maroto García", "Consultas de prueba","B02", false));
+        this.medconRepository.save(new Consulta( 13 ,"30/3/2022", "Ramón","Juan Carlos Yelmo García", "Consultas de prueba","G54", false));
+        this.medconRepository.save(new Consulta( 2 ,"30/3/2022", "Ramón","Valentín de la Rubia Hernández", "Consultas de prueba","F05", true));
+        this.medconRepository.save(new Consulta( 1 ,"30/3/2022", "Ramón","Marco César Maicas Ramos", "Consultas de prueba","P67", true));
+        this.medconRepository.save(new Consulta( 0 ,"30/3/2022", "Ramón","Diego Martín de Andrés", "Consultas de prueba","D03", true));
+        this.medconRepository.save(new Consulta( 3 ,"30/3/2022", "Ramón","Mateo Burgos García", "Consultas de prueba","U98", true));
+        this.medconRepository.save(new Consulta( 4 ,"30/3/2022", "Ramón","Benito Artaloytia Encinas", "Consultas de prueba","K36", true));
+        this.medconRepository.save(new Consulta( 5 ,"30/3/2022", "Ramón","Pablo Sánchez Olivares", "Consultas de prueba","G06", true));
+        this.medconRepository.save(new Consulta( 6 ,"30/3/2022", "Ramón","Luis Mendo Tomas", "Consultas de prueba","L89", true));
     }
     @GetMapping("/consultas")
     List<Consulta> readAll(){
