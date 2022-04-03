93e72ac8db771e328b069956b148c836972166a8
andreshere00 <andresherencia2000@gmail.com>
2022-04-03 14:01:21 +0200

    Cambios en la definición de la base de datos

--- src/main/java/es/upm/dit/isst/medcon/persistence/controller/MedconController.java
+++ src/main/java/es/upm/dit/isst/medcon/persistence/controller/MedconController.java
@@ -1,16 +1,16 @@
 package es.upm.dit.isst.medcon.persistence.controller;
-import java.net.URI;
-import java.net.URISyntaxException;
+//import java.net.URI;
+//import java.net.URISyntaxException;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import es.upm.dit.isst.medcon.persistence.repository.MedconRepository;
 import es.upm.dit.isst.medcon.persistence.repository.PacientesRepository;
 import es.upm.dit.isst.medcon.persistence.model.Consulta;
-import es.upm.dit.isst.medcon.persistence.model.Medico;
+//import es.upm.dit.isst.medcon.persistence.model.Medico;
 import es.upm.dit.isst.medcon.persistence.model.Paciente;
 
 @RestController
@@ -28,34 +28,34 @@ public class MedconController {
     public static final Logger log = LoggerFactory.getLogger(MedconController.class);
     public MedconController(MedconRepository t, PacientesRepository p){
         this.pacientesRepository = p;
-        this.pacientesRepository.save(new Paciente( 185672890, "78653477H", "Diego Martín de Andrés", new Integer[] {0}));
-        this.pacientesRepository.save(new Paciente( 225522890, "45435698D", "Marco César Maicas Ramos", new Integer[] {1}));
-        this.pacientesRepository.save(new Paciente( 396756288, "86456774F", "Valentín de la Rubia Hernández", new Integer[] {2}));
-        this.pacientesRepository.save(new Paciente( 489272890, "70454325F", "Mateo Burgos García", new Integer[] {3}));
-        this.pacientesRepository.save(new Paciente( 537283790, "89573369H", "Benito Artaloytia Encinas", new Integer[] {4}));
-        this.pacientesRepository.save(new Paciente( 693872290, "90643568D", "Pablo Sánchez Olivares", new Integer[] {5}));
-        this.pacientesRepository.save(new Paciente( 734333210, "69292286G", "Luis Mendo Tomas", new Integer[] {6}));
-        this.pacientesRepository.save(new Paciente( 834567890, "45367876F", "María Asunción Santamaría Galdón", new Integer[] {7}));
-        this.pacientesRepository.save(new Paciente( 917656435, "76262816D", "Jesús Frayle Ardanuy", new Integer[] {8}));
-        this.pacientesRepository.save(new Paciente( 924654278, "72698765D", "Víctor Abraham Villagrá González", new Integer[] {9}));
-        this.pacientesRepository.save(new Paciente( 937652288, "98224684F", "Gabriel Huecas Toribio", new Integer[] {10}));
-        this.pacientesRepository.save(new Paciente( 946546892, "54378086E", "María de la Nava Maroto García", new Integer[] {11}));
-        this.pacientesRepository.save(new Paciente( 987652728, "93214897H", "Juan Carlos Yelmo García", new Integer[] {12}));
+        p.save(new Paciente( 185672890, "78653477H", "Diego Martín de Andrés", new Integer[] {0}));
+        p.save(new Paciente( 225522890, "45435698D", "Marco César Maicas Ramos", new Integer[] {1}));
+        p.save(new Paciente( 396756288, "86456774F", "Valentín de la Rubia Hernández", new Integer[] {2}));
+        p.save(new Paciente( 489272890, "70454325F", "Mateo Burgos García", new Integer[] {3}));
+        p.save(new Paciente( 537283790, "89573369H", "Benito Artaloytia Encinas", new Integer[] {4}));
+        p.save(new Paciente( 693872290, "90643568D", "Pablo Sánchez Olivares", new Integer[] {5}));
+        p.save(new Paciente( 734333210, "69292286G", "Luis Mendo Tomas", new Integer[] {6}));
+        p.save(new Paciente( 834567890, "45367876F", "María Asunción Santamaría Galdón", new Integer[] {7}));
+        p.save(new Paciente( 917656435, "76262816D", "Jesús Frayle Ardanuy", new Integer[] {8}));
+        p.save(new Paciente( 924654278, "72698765D", "Víctor Abraham Villagrá González", new Integer[] {9}));
+        p.save(new Paciente( 937652288, "98224684F", "Gabriel Huecas Toribio", new Integer[] {10}));
+        p.save(new Paciente( 946546892, "54378086E", "María de la Nava Maroto García", new Integer[] {11}));
+        p.save(new Paciente( 987652728, "93214897H", "Juan Carlos Yelmo García", new Integer[] {12}));
 
         this.medconRepository = t;
-        this.medconRepository.save(new Consulta( "7" ,"30/3/2022", "Ramón","María Asunción Santamaría Galdón", "Consultas de prueba","A01", false));
-        this.medconRepository.save(new Consulta( "8" ,"30/3/2022", "Ramón","Jesús Frayle Ardanuy", "Consultas de prueba","D03", false));
-        this.medconRepository.save(new Consulta( "9" ,"30/3/2022", "Ramón","Víctor Abraham Villagrá González", "Consultas de prueba","H69", false));
-        this.medconRepository.save(new Consulta( "10" ,"30/3/2022", "Ramón","Gabriel Huecas Toribio", "Consultas de prueba","X43", false));
-        this.medconRepository.save(new Consulta( "11" ,"30/3/2022", "Ramón","María de la Nava Maroto García", "Consultas de prueba","B02", false));
-        this.medconRepository.save(new Consulta( "12" ,"30/3/2022", "Ramón","Juan Carlos Yelmo García", "Consultas de prueba","G54", false));
-        this.medconRepository.save(new Consulta( "2" ,"30/3/2022", "Ramón","Valentín de la Rubia Hernández", "Consultas de prueba","F05", true));
-        this.medconRepository.save(new Consulta( "1" ,"30/3/2022", "Ramón","Marco César Maicas Ramos", "Consultas de prueba","P67", true));
-        this.medconRepository.save(new Consulta( "0" ,"30/3/2022", "Ramón","Diego Martín de Andrés", "Consultas de prueba","D03", true));
-        this.medconRepository.save(new Consulta( "3" ,"30/3/2022", "Ramón","Mateo Burgos García", "Consultas de prueba","U98", true));
-        this.medconRepository.save(new Consulta( "4" ,"30/3/2022", "Ramón","Benito Artaloytia Encinas", "Consultas de prueba","K36", true));
-        this.medconRepository.save(new Consulta( "5" ,"30/3/2022", "Ramón","Pablo Sánchez Olivares", "Consultas de prueba","G06", true));
-        this.medconRepository.save(new Consulta( "6" ,"30/3/2022", "Ramón","Luis Mendo Tomas", "Consultas de prueba","L89", true));
+        t.save(new Consulta( "7" ,"30/3/2022", "Ramón","María Asunción Santamaría Galdón", "Consultas de prueba","A01", false));
+        t.save(new Consulta( "8" ,"30/3/2022", "Ramón","Jesús Frayle Ardanuy", "Consultas de prueba","D03", false));
+        t.save(new Consulta( "9" ,"30/3/2022", "Ramón","Víctor Abraham Villagrá González", "Consultas de prueba","H69", false));
+        t.save(new Consulta( "10" ,"30/3/2022", "Ramón","Gabriel Huecas Toribio", "Consultas de prueba","X43", false));
+        t.save(new Consulta( "11" ,"30/3/2022", "Ramón","María de la Nava Maroto García", "Consultas de prueba","B02", false));
+        t.save(new Consulta( "12" ,"30/3/2022", "Ramón","Juan Carlos Yelmo García", "Consultas de prueba","G54", false));
+        t.save(new Consulta( "2" ,"30/3/2022", "Ramón","Valentín de la Rubia Hernández", "Consultas de prueba","F05", true));
+        t.save(new Consulta( "1" ,"30/3/2022", "Ramón","Marco César Maicas Ramos", "Consultas de prueba","P67", true));
+        t.save(new Consulta( "0" ,"30/3/2022", "Ramón","Diego Martín de Andrés", "Consultas de prueba","D03", true));
+        t.save(new Consulta( "3" ,"30/3/2022", "Ramón","Mateo Burgos García", "Consultas de prueba","U98", true));
+        t.save(new Consulta( "4" ,"30/3/2022", "Ramón","Benito Artaloytia Encinas", "Consultas de prueba","K36", true));
+        t.save(new Consulta( "5" ,"30/3/2022", "Ramón","Pablo Sánchez Olivares", "Consultas de prueba","G06", true));
+        t.save(new Consulta( "6" ,"30/3/2022", "Ramón","Luis Mendo Tomas", "Consultas de prueba","L89", true));
     }
     /**
      * API GET que devuelve la lista completa de consultas con sus atributos.
