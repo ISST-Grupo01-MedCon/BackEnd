4d9a1ce0acddf4fd4d61f09c70db42909a882246
Ramón Hernández García <rmnhg16@gmail.com>
2022-05-03 21:47:56 +0200

    Resolución de errores pruebas JUnit * Se han añadido @Autowired a repo2 y repo3 para que SpringBoot los inicialice como hace con repo. * Se han redefinido los tests testPaciente() y testMedico() para usar findByAlgo(), que cuando no son findById() devuelven una List en vez de un Optional.

--- src/test/java/es/upm/dit/isst/medcon/MedconApplicationTests.java
+++ src/test/java/es/upm/dit/isst/medcon/MedconApplicationTests.java
@@ -3,6 +3,7 @@ package es.upm.dit.isst.medcon;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
@@ -10,7 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import antlr.collections.List;
 import es.upm.dit.isst.medcon.persistence.model.Consulta;
 import es.upm.dit.isst.medcon.persistence.model.Medico;
 import es.upm.dit.isst.medcon.persistence.model.Paciente;
@@ -23,7 +23,9 @@ class MedconApplicationTests {
 
 	@Autowired
     private MedconRepository repo;
+	@Autowired
 	private PacientesRepository repo2;
+	@Autowired
 	private MedicosRepository repo3;
 
 	@Test
@@ -54,6 +56,7 @@ class MedconApplicationTests {
         con2 = repo.findById("7");
         assertFalse(con2.isPresent());
 	}
+
 	@Test
 	final void testPaciente() {
 		Paciente pac = new Paciente();
@@ -61,23 +64,22 @@ class MedconApplicationTests {
 		pac.setDni("78653477H");
 		pac.setNombre("Diego Martín de Andrés");
 		pac.setCitas(new Integer[] {0});
-
 		repo2.save(pac);
 
-		Optional<Paciente> pac2 = repo2.findById("185672890");
+		Paciente pac2 = repo2.findByCipa(185672890).get(0);
 
-		assertEquals(pac2.get().getCipa(), pac.getCipa());
-		assertEquals(pac2.get().getDni(), "78653477H");
-		assertEquals(pac2.get().getNombre(), "Diego Martín de Andrés");
+		assertEquals(pac2.getCipa(), pac.getCipa());
+		assertEquals(pac2.getDni(), "78653477H");
+		assertEquals(pac2.getNombre(), "Diego Martín de Andrés");
 
 		pac.setCitas(new Integer[] {0, 1});
         repo2.save(pac);
 
-		assertNotEquals(pac2.get().getCitas(), pac.getCitas());	
+		assertNotEquals(pac2.getCitas(), pac.getCitas());	
 		repo2.delete(pac);
-		pac2 = repo2.findById("185672890");
-		assertFalse(pac2.isPresent());
+		assertTrue(repo2.findByCipa(185672890).isEmpty());
 	}
+
 	@Test
 	final void testMedico() {
 		Medico med = new Medico();
@@ -88,9 +90,9 @@ class MedconApplicationTests {
 
 		repo3.save(med);
 
-		Medico med1 = (Medico) repo3.findByUsuario("111222333");
+		Medico med1 = (Medico) repo3.findByUsuario("111222333").get(0);
 
-		assertEquals(med1.getNombre(), med.getContraseña());
+		assertEquals(med1.getNombre(), "Ramón");
 		assertEquals(med1.getContraseña(), "MeEncantaISST");
 
 		med1.setContraseña("nuevaCon");
