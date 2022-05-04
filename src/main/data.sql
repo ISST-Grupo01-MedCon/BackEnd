8194dbd24c0d4ee8dcff88f504c3751424b4c50b
pedrolf8 <80721931+pedrolf8@users.noreply.github.com>
2022-05-04 15:19:14 +0200

    Update data.sql

--- src/main/data.sql
+++ src/main/data.sql
@@ -1,2 +1,2 @@
-insert into users(username, password, enabled) values ('medico', '{noop}medico1', true);
-insert into authorities(username, authority) values ('medico', 'MEDICO');
+insert into users(username, password, enabled) values ('111222333', '{noop}MeEncantaISST', true);
+insert into authorities(username, authority) values ('111222333', 'MEDICO');
